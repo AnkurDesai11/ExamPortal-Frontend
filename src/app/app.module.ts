feb8330f9bb9846dba21cb5df4b8f1b1c88ea9a3
AnkurDesai11 <60006478+AnkurDesai11@users.noreply.github.com>
2021-06-11 01:33:58 +0530

    Registration module, login page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HomeComponent } from './pages/home/home.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  public user = {
+    username: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+  };
+
+  constructor(private userService: UserService, private snack: MatSnackBar) { }
+
+  ngOnInit(): void {
+  }
+
+  formSubmit() {
+    //adduser function call
+    let regexpcontact = new RegExp('^[- +()0-9]{6,15}$');
+    let regexppassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&]).{8,}');
+
+    if (!regexppassword.test(this.user.password)) {
+      this.snack.open("Password should contain atleast one of the following symbols - ' ! @ # $ % ^ & ' one uppercase, one lowercase letter, one number and should be atleast 8 characters long", "OK", { verticalPosition: "top" });
+      return;
+    }
+
+    if ((this.user.phone != "" && this.user.phone != null) && !regexpcontact.test(this.user.phone)) {
+      this.snack.open("Enter valid contact number", "OK", { duration: 2000, verticalPosition: "top" });
+      return;
+    }
+
+    console.log(this.user);
+    this.userService.addUser(this.user).subscribe(
+      (data: any) => {
+        //success
+        console.log(data);
+        Swal.fire("Success", "User registered with ID " + data.id, "success");
+      },
+      (error) => {
+        //error
+        console.log(error);
+        //lert("error");
+        this.snack.open("Error, please try again", "", { duration: 2000, verticalPosition: "top" })
+      }
+    )
+  }
+
+}
